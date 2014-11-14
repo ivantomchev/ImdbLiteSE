37478c6c0b0436798fb94bc9649b7e4a27c8ddd2
ivantomchev <ivantomchev@gmail.com>
2014-11-14 21:33:37 +0200

    Seed some genres

--- Source/Data/ImdbLite.Data/Migrations/Configuration.cs
+++ Source/Data/ImdbLite.Data/Migrations/Configuration.cs
@@ -1,6 +1,8 @@
 namespace ImdbLite.Data.Migrations
 {
+    using ImdbLite.Data.Models.Genre;
     using System;
+    using System.Collections.Generic;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
     using System.Linq;
@@ -15,7 +17,44 @@ namespace ImdbLite.Data.Migrations
 
         protected override void Seed(ApplicationDbContext context)
         {
+            PopulateGenres(context);
 
+            context.SaveChanges();
+
+            base.Seed(context);
+        }
+
+        static void PopulateGenres(ApplicationDbContext context)
+        {
+            var genres = new List<Genre>()
+            {
+                new Genre {Type = "Action"},
+                new Genre{Type = "Adventure"},
+                new Genre{Type = "Animation"},
+                new Genre{Type = "Biography"},
+                new Genre{Type = "Comedy"},
+                new Genre{Type = "Crime"},
+                new Genre{Type = "Documentary"},
+                new Genre {Type = "Drama"},
+                new Genre{Type = "Family"},
+                new Genre{Type = "Fantasy"},
+                new Genre{Type = "FilmNoir"},
+                new Genre{Type = "Horror"},
+                new Genre{Type = "History"},
+                new Genre{Type = "Music"},
+                new Genre {Type = "Musical"},
+                new Genre{Type = "Mystery"},
+                new Genre{Type = "Romance"},
+                new Genre{Type = "SciFi"},
+                new Genre{Type = "Short"},
+                new Genre{Type = "Sport"},
+                new Genre{Type = "Thriller"},
+                new Genre{Type = "War"},
+                new Genre{Type = "Western"},
+       
+            };
+
+            genres.ForEach(g => context.Genres.AddOrUpdate(p => p.Type, g));
         }
     }
 }
