23743d82cfa28dd072fcf170ef621912827e49a6
ivantomchev <ivantomchev@gmail.com>
2014-11-15 13:34:44 +0200

    Added ActorsController in Administration Area;

---
+++ Source/Web/ImdbLite.Web/Areas/Administration/Controllers/ActorsController.cs
@@ -0,0 +1,22 @@
+﻿namespace ImdbLite.Web.Areas.Administration.Controllers
+{
+    using ImdbLite.Data.Common.Repository;
+    using ImdbLite.Data.Models.Actor;
+    using System.Web.Mvc;
+
+    public class ActorsController : Controller
+    {
+        private readonly IDeletableEntityRepository<Actor> actors;
+
+        public ActorsController(IDeletableEntityRepository<Actor> actors)
+        {
+            this.actors = actors;
+        }
+
+        // GET: Administration/Actors
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
