abbc89746b8f5537c0ee4e5f373e7be8df16c6c9
ivantomchev <ivantomchev@gmail.com>
2014-11-20 12:44:51 +0200

    Disable unused view engines.

--- Source/Web/ImdbLite.Web/Global.asax.cs
+++ Source/Web/ImdbLite.Web/Global.asax.cs
@@ -19,6 +19,9 @@
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
 
+            ViewEngines.Engines.Clear();
+            ViewEngines.Engines.Add(new RazorViewEngine());
+
             var autoMapperConfig = new AutoMapperConfig(Assembly.GetExecutingAssembly());
             autoMapperConfig.Execute();
         }
