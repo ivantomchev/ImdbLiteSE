802161473e2ca1e96f22131cb911843639642dd3
Ivan Tomchev <ivantomchev@gmail.com>
2015-02-26 13:21:30 +0200

    Input Model for CastMembers Created
    Input Model for CastMembers Created

---
+++ Source/Web/ImdbLite.Web/Areas/Administration/ViewModels/CastMembers/CastMemberInputModel.cs
@@ -0,0 +1,22 @@
+﻿namespace ImdbLite.Web.Areas.Administration.ViewModels.CastMembers
+{
+    using System.Web.Mvc;
+
+    using ImdbLite.Web.Infrastructure.Mapping;
+    using ImdbLite.Data.Models;
+
+    public class CastMemberInputModel : IMapFrom<CastMember>
+    {
+        [HiddenInput]
+        public int Id { get; set; }
+
+        [HiddenInput]
+        public int CelebrityId { get; set; }
+
+        [HiddenInput]
+        public ParticipationType Participation { get; set; }
+
+        [HiddenInput]
+        public int MovieId { get; set; }
+    }
+}
