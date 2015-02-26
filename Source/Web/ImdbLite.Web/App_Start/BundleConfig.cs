a4c6daaf018437622bcc4d945cddd8cf412ab891
Ivan Tomchev <ivantomchev@gmail.com>
2015-02-26 12:48:47 +0200

    Update BundleConfig
    Update BundleConfig, movies-create.js (useless) deleted

--- Source/Web/ImdbLite.Web/App_Start/BundleConfig.cs
+++ Source/Web/ImdbLite.Web/App_Start/BundleConfig.cs
@@ -19,7 +19,12 @@ namespace ImdbLite.Web
         private static void RegisterStyleBundles(BundleCollection bundles)
         {
             bundles.Add(new StyleBundle("~/Content/css").Include(
-                      "~/Content/bootstrap.darkly.css"));
+                      "~/Scripts/Chosen/chosen.css",
+                      "~/Scripts/fancybox/jquery.fancybox.css",
+                      "~/Scripts/fancybox/jquery.fancybox-buttons.css",
+                      "~/Scripts/fancybox/jquery.fancybox-thumbs.css",
+                      "~/Content/bootstrap.darkly.css",
+                      "~/Content/Site.css"));
 
             bundles.Add(new StyleBundle("~/Content/custom").Include(
                 "~/Content/Site.css"));
@@ -27,6 +32,30 @@ namespace ImdbLite.Web
 
         private static void RegisterScriptBundles(BundleCollection bundles)
         {
+            bundles.Add(new ScriptBundle("~/bundles/fancybox").Include(
+                      "~/Scripts/fancybox/jquery.fancybox.pack.js",
+                      "~/Scripts/fancybox/helpers/jquery.fancybox-buttons.js",
+                      "~/Scripts/fancybox/helpers/jquery.fancybox-media.js",
+                      "~/Scripts/fancybox/helpers/jquery.fancybox-thumbs.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/picture-preview").Include(
+            "~/Scripts/custom/picture-preview.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/movies-create-server-side").Include(
+            "~/Scripts/custom/movies-create-server-side.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/movies-delete").Include(
+            "~/Scripts/custom/movies-delete.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/genres-delete").Include(
+            "~/Scripts/custom/genres-delete.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/celebrities-delete").Include(
+            "~/Scripts/custom/celebrities-delete.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/crudCelebrities").Include(
+            "~/Scripts/Modal/crudCelebrities.js"));
+
             bundles.Add(new ScriptBundle("~/bundles/modal").Include(
                         "~/Scripts/Modal/modalform.js"));
 
@@ -44,6 +73,9 @@ namespace ImdbLite.Web
             bundles.Add(new ScriptBundle("~/bundles/bootstrap").Include(
                       "~/Scripts/bootstrap.js",
                       "~/Scripts/respond.js"));
+
+            bundles.Add(new ScriptBundle("~/bundles/Chosen").Include(
+                        "~/Scripts/chosen/chosen.jquery.js"));
         }
     }
 }
